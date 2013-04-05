73e560e8fdc4e3c524cfed55a6d74ea1f471f600
Bitmaker Labs <bitmakerlabs@bitmaker01.local>
2013-04-05 13:44:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mchammer::Application.config.secret_token = '6e90302540a2d2cb4789bd8e6308b715c1a7777c21f3a81942bed02f5b047435b111ee0c21bd5e3ac487f27bfcb88539ce9e5cd02a1d09a0b3d3518cced0f2a1'
