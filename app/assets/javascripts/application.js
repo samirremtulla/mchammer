450ac4d5943cd70eaee8a27de4396628addcce6e
Bitmaker Labs <bitmakerlabs@bitmaker01.local>
2013-04-05 15:17:06 -0400

    added the ajax to control the robot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,40 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+$(function(){
+	$(".btn-success").on("click", function() {
+		console.log("Forward");
+		$.ajax({url: "127.0.0.1:8071/motion-control/update", data: {forward: 1}, dataType: "jsonp"});
+	}); 
+
+	$(".btn-danger").on("click", function() {
+		console.log("Reverse");
+		$.ajax({url: "127.0.0.1:8071/motion-control/update", data: {forward: -1}, dataType: "jsonp"});
+	}); 
+
+	$(".btn-info").on("click", function() {
+		console.log("Left");
+		$.ajax({url: "127.0.0.1:8071/motion-control/update", data: {turn: -1}, dataType: "jsonp"});
+		
+	}); 
+
+	$(".btn-warning").on("click", function() {
+		console.log("right");
+		$.ajax({url: "127.0.0.1:8071/motion-control/update", data: {turn: 1}, dataType: "jsonp"});
+		//ajax
+	});
+
+	$(".left").on("click", function() {
+		console.log("SL");
+		//ajax
+		$.ajax({url: "127.0.0.1:8071/motion-control/update", data: {strafe: -1}, dataType: "jsonp"});
+	});
+
+	$(".right").on("click", function() {
+		console.log("SR");
+		$.ajax({url: "127.0.0.1:8071/motion-control/update", data: {strafe: 1}, dataType: "jsonp"});
+		//ajax
+	});
+
+});
